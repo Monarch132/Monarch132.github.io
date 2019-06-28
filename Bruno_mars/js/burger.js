569196180ce6c420eaf81b09d7941e0fca7dfd93
Monarch132 <mirenkov.alexander@ya.ru>
2019-06-28 23:59:07 +0300

    drop_menu

--- Bruno_mars/js/burger.js
+++ Bruno_mars/js/burger.js
@@ -1,7 +1,9 @@
 
     const menuThree = document.querySelector('.menuThree');
-  
+    const slideMenu = document.querySelector('.slide_menu');
+
     function addClassFunThree() {
         this.classList.toggle("clickMenuThree");
+        slideMenu.classList.toggle("hiden");
     }
     menuThree.addEventListener('click', addClassFunThree);
